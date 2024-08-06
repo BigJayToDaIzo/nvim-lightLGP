d4c79a3d52790c7ab22a4cba8f22ddb842683f3e
Joseph Myers <josephlmyers@gmail.com>
2024-08-06 12:44:49 -0500

    keymaps notes tidied up

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 -- NOTE: Where it all begins.
 -- 1) Load basic vim options prior to ANYTHING else
+-- init.lua already knows about the lua directory below it
+-- require'config.vimpots' is importing lua/config/vimopts.lua here
 require("config.vimopts")
 -- 2) Set non plugin specific keymaps
-require("config.keymaps")
+require("config.keymaps") -- import lua/config.keymaps.lua here
 -- 3) Load Lazy Plugin Manager
-require("config.lazy")
+require("config.lazy") -- guess what's happening here.
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,7 +15,6 @@ vim.keymap.set("n", "<C-z>", "<Nop>")
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set({ "n", "x" }, "<M-Space>", "<cmd>WhichKey<CR>", { desc = "Open WhichKey menu" })
 --Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
@@ -24,31 +23,30 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- map leaders here so plugins quit screwing things up?
 -- NOTE: Global notes on which plugins control which keychord sets aka <leader>C
 -- where C is the chord the subcommands will dictate
--- NOTE: Gitsigns probably going to need a chord. <leader>g*
+-- Map leaders here so mini.clue quits screwing things up?
 -- mini.choice is NOT playing well with gitsigns binds NOR my space leader
 -- this hack is necessary for it to group <leader>g binds to Gitsigns
+--
+-- NOTE: Gitsigns will take chord <leader>g*
 vim.keymap.set("n", "<leader>g", function() end, { desc = "+Gitsigns" })
--- NOTE: Oil unchorded using <leader>e (may need to reclaim to chord something)
--- NOTE: Debug/Diagnostic chorded under <leader>d*
--- NOTE: Telescope chorded under <leader>s* (may need to be broadened to <leader>t)
--- NOTE: Todo need to be chorded? --ok with :Todo<C-d> for now!
--- Starting to feel like a need for a chord for
--- :lua require'todo-comments'.jump_next/prev()
--- NOTE: Completion chorded under <leader>? --More like just check collisions and readiness in
--- <leader>sk (search keybind is your command palette!)
--- NOTE: Bufferline claims <leader>b*
--- UPDATE IT after you install plugins with default bindings! (or want to tweak your own without collisions)
--- NOTE: Notifications Dismiss takex <leader>x (for now)
--- NOTE: LSP binds get their own <leader>l
--- FIX: Chords claimed thus far <leader>[b, d, e, g, l, s, x] (update array as we add things)
--- NOTE: We now have which-key hints, so groups in <leader> array
--- will need to be added to the clues table there or here if that doesnt work correctly
 
--- -- Precognition binds
--- vim.keymap.set("n", "<leader>pt", "<cmd>lua require('precognition').toggle()<cr>", { desc = "[P]recognition [T]oggle" })
--- vim.keymap.set("n", "<leader>pp", "<cmd>lua require('precognition').peek()<cr>", { desc = "[P]recognition [P]eek" })
--- vim.keymap.set("n", "<leader>ps", "<cmd>lua require('precognition').show()<cr>", { desc = "[P]recognition [S]how" })
--- vim.keymap.set("n", "<leader>ph", "<cmd>lua require('precognition').hide()<cr>", { desc = "[P]recognition [H]ide" })
+-- NOTE: Oil unchorded using <leader>e (you may want to reclaim to chord something else, or use a different bind entirely)
+--
+-- NOTE: Debug/Diagnostic chorded under <leader>d*
+--
+-- NOTE: Telescope chorded under <leader>s*
+-- PERF:<leader>sk (search keybind is your command palette!)
+--
+-- NOTE: Todo need to be chorded? --ok with :Todo<C-d> for now! or <leader>st
+--
+-- NOTE: Bufferline claims <leader>b*
+--
+-- NOTE: Notifications Dismiss takes <leader>x (for now)
+--
+-- NOTE: LSP chorded under <leader>l
+--
+-- NOTE: Precognition is simple enough to run from cmdline. :Precognition <C-d>
+--
+-- FIX: Chords claimed thus far <leader>[b, d, e, g, l, s, x] (update array as we add things)
